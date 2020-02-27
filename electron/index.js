324cfdd444637be552cc66776138956b05ccba5a
Richard Peng <richard@richardpeng.com>
2020-02-27 09:11:11 -0500

    Embed Next server in Electron

--- electron/index.js
+++ electron/index.js
@@ -1,5 +1,16 @@
 const { app, BrowserWindow } = require('electron')
-const nextApp = require('./server')
+// const nextApp = require('./server')
+const unhandled = require('electron-unhandled');
+
+unhandled();
+
+let serverProc = require('child_process').fork(
+  require.resolve('./server'),
+  [], // pass to process.argv into child
+  {
+    // options
+  }
+)
 
 const isDev = require('electron-is-dev')
 
@@ -13,39 +24,37 @@ function createWindow () {
     }
   })
 
-  // and load the index.html of the app.
-  win.loadURL('http://localhost:3000')
+  // win.loadFile('src/index.html')
+  // nextApp(false).then(() => {
+    win.loadURL('http://localhost:3000')
 
-  // Open the DevTools.
-  if (isDev) {
-    win.webContents.openDevTools()
-  }
+    if (isDev) {
+      win.webContents.openDevTools()
+    }
+  // });
 }
 
-nextApp(isDev).then(() => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-  app.whenReady().then(createWindow)
+app.whenReady().then(createWindow)
 
 // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
-  })
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-});
-
--- electron/server.js
+++ electron/server.js
@@ -5,27 +5,21 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
-module.exports = function (dev) {
-  const app = next({ dev })
+console.log('server loading')
+// module.exports = function (dev) {
+  const app = next({ dev: false })
   const handle = app.getRequestHandler()
 
-  return app.prepare().then(() => {
-    return createServer((req, res) => {
+  app.prepare().then(() => {
+    createServer((req, res) => {
       // Be sure to pass `true` as the second argument to `url.parse`.
       // This tells it to parse the query portion of the URL.
       const parsedUrl = parse(req.url, true)
-      const { pathname, query } = parsedUrl
 
-      if (pathname === '/a') {
-        app.render(req, res, '/b', query)
-      } else if (pathname === '/b') {
-        app.render(req, res, '/a', query)
-      } else {
-        handle(req, res, parsedUrl)
-      }
+      handle(req, res, parsedUrl)
     }).listen(3000, err => {
       if (err) throw err
       console.log('> Ready on http://localhost:3000')
     })
   })
-}
+// }
